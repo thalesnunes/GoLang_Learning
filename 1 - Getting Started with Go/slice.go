da1780050fb0db3368db8ca8db5606e94ceb7e23
Thales Nunes <thalesaknunes22@gmail.com>
2020-08-06 20:05:33 -0300

    slice

---
+++ 1 - Getting Started with Go/slice.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
+func main() {
+
+	sli := make([]int, 0)
+	var txt string
+	var ap int
+	for {
+		fmt.Scan(&txt)
+		if txt == "X" {
+			break
+		}
+		ap, _ = strconv.Atoi(txt)
+		sli = append(sli, ap)
+	}
+	sort.SliceStable(sli, func(i, j int) bool { return sli[i] < sli[j] })
+	for _, val := range sli {
+		fmt.Printf("%d ", val)
+	}
+}
